4178360728f14fc9bd2d96bd7eefe9375df79615
Vladimir Agafonkin <agafonkin@gmail.com>
2014-09-27 01:11:49 +0300

    add lots of benchmarks

--- bench.js
+++ bench.js
@@ -1,18 +1,60 @@
 
 var Benchmark = require('benchmark'),
-    bbtree = require('./bbtree');
+    bbtree = require('./bbtree'),
+    dsjslib = require('dsjslib'),
+    bintrees = require('bintrees');
+    binarySearchTree = require('binary-search-tree'),
+    functionalRBTree = require('functional-red-black-tree');
 
-var data = [];
+var data = [],
+    N = 1000;
 
-for (var i = 0; i < 1000; i++) {
-	data[i] = Math.floor(Math.random() * 1000);
+for (var i = 0; i < N; i++) {
+    data[i] = Math.floor(Math.random() * N);
 }
 
-var tree = bbtree().load(data);
+console.log('insert ' + N + ' items:');
+
+function compare(a, b) {
+    return a < b ? -1 : a > b ? 1 : 0;
+}
 
 new Benchmark.Suite()
-.add('insert 10000 items', function () {
-    bbtree().load(data);
+.add('bbtree', function () {
+    var tree = bbtree(compare);
+    for (var i = 0; i < N; i++) {
+        tree.insert(data[i]);
+    }
+})
+.add('functional-red-black-tree', function () {
+    var tree = functionalRBTree(compare);
+    for (var i = 0; i < N; i++) {
+        tree.insert(data[i]);
+    }
+})
+.add('jsbintrees RBTree', function () {
+    var tree = new bintrees.RBTree(compare);
+    for (var i = 0; i < N; i++) {
+        tree.insert(data[i]);
+    }
+})
+.add('dsjslib AVLTree', function () {
+    var tree = new dsjslib.AVLTree(compare);
+    for (var i = 0; i < N; i++) {
+        tree.put(data[i]);
+    }
+})
+.add('dsjslib SkipList', function () {
+    var tree = new dsjslib.SkipList(compare);
+    for (var i = 0; i < N; i++) {
+        tree.put(data[i]);
+    }
+})
+.add('binary-search-tree AVLTree', function () {
+    var tree = new binarySearchTree.AVLTree({compareKeys: compare});
+    for (var i = 0; i < N; i++) {
+        tree.insert(data[i]);
+    }
 })
 .on('error', function(event) {
     console.log(event.target.error);
