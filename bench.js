05b3da6148b7b36a980042fd471f0cc2981c8aa6
Vladimir Agafonkin <agafonkin@gmail.com>
2014-09-28 11:18:37 +0300

    add bsarray for perf comparisons

--- bench.js
+++ bench.js
@@ -2,13 +2,12 @@
 var Benchmark = require('benchmark'),
     bbtree = require('./bbtree'),
     functionalRBTree = require('functional-red-black-tree'),
-    bintrees = require('bintrees');
-    // not benchmarking these, they're slow and so not interesting
-    // dsjslib = require('dsjslib'),
-    // binarySearchTree = require('binary-search-tree'),
+    bintrees = require('bintrees'),
+    bsarray = require('./bsarray');
+    // not benchmarking dsjslib & binarySearchTree, they're slow and so not interesting
 
 var data = [],
-    N = 1000;
+    N = 5000;
 
 for (var i = 0; i < N; i++) {
     data[i] = Math.floor(Math.random() * N);
@@ -27,6 +26,12 @@ new Benchmark.Suite()
         tree.insert(data[i]);
     }
 })
+.add('bsarray', function () {
+    var arr = bsarray(compare);
+    for (var i = 0; i < N; i++) {
+        arr.insert(data[i]);
+    }
+})
 .add('functional-red-black-tree', function () {
     var tree = functionalRBTree(compare);
     for (var i = 0; i < N; i++) {
@@ -39,24 +44,6 @@ new Benchmark.Suite()
         tree.insert(data[i]);
     }
 })
-// .add('dsjslib AVLTree', function () {
-//     var tree = new dsjslib.AVLTree(compare);
-//     for (var i = 0; i < N; i++) {
-//         tree.put(data[i]);
-//     }
-// })
-// .add('dsjslib SkipList', function () {
-//     var tree = new dsjslib.SkipList(compare);
-//     for (var i = 0; i < N; i++) {
-//         tree.put(data[i]);
-//     }
-// })
-// .add('binary-search-tree AVLTree', function () {
-//     var tree = new binarySearchTree.AVLTree({compareKeys: compare});
-//     for (var i = 0; i < N; i++) {
-//         tree.insert(data[i]);
-//     }
-// })
 .on('error', function(event) { console.log(event.target.error); })
 .on('cycle', function(event) { console.log(String(event.target)); }).run();
 
@@ -78,6 +65,11 @@ for (var i = 0; i < N; i++) {
     bintree.insert(data[i]);
 }
 
+var arr = bsarray(compare);
+for (var i = 0; i < N; i++) {
+    arr.insert(data[i]);
+}
+
 function randomIndex(N) {
     return Math.floor(Math.random() * N);
 }
@@ -85,6 +77,9 @@ function randomIndex(N) {
 new Benchmark.Suite()
 .add('bbtree', function () {
     btree.find(data[randomIndex(N)]);
+})
+.add('bsarray', function () {
+    arr.find(data[randomIndex(N)]);
 })
 .add('functional-red-black-tree', function () {
     rbtree.get(data[randomIndex(N)]);
---
+++ bsarray.js
@@ -0,0 +1,69 @@
+
+// really simple pseudo-bst array structure
+
+module.exports = function bsarray(compare) {
+    return new BSArray(compare);
+};
+
+function BSArray(compare) {
+    this._compare = compare || defaultCompare;
+    this.items = [];
+}
+
+function defaultCompare(a, b) {
+    return a < b ? -1 :
+           a > b ?  1 : 0;
+}
+
+BSArray.prototype = {
+
+    insert: function (key, value) {
+
+        var items = this.items,
+            compare = this._compare,
+            i = 0,
+            j = items.length - 1;
+
+        while (true) {
+            if (j > i) {
+                var pivot = Math.floor((i + j) / 2);
+                var c = compare(key, items[pivot].key);
+                if (c === 0) return false;
+                if (c < 0) j = pivot - 1;
+                else i = pivot + 1;
+
+            } else break;
+        }
+
+        if (items.length) {
+            var c = compare(key, items[i].key);
+            if (c === 0) return false;
+            if (c > 0) i++;
+        } else i = 0;
+
+        items.splice(i, 0, {key: key, value: value});
+
+        return true;
+    },
+
+    find: function (key) {
+
+        var items = this.items,
+            compare = this._compare,
+            i = 0,
+            j = items.length - 1;
+
+        while (true) {
+            var pivot = Math.floor((i + j) / 2);
+            var c = compare(key, items[pivot].key);
+            if (c === 0) return pivot;
+            if (j <= i) return null;
+            if (c < 0) j = pivot - 1;
+            else i = pivot + 1;
+        }
+    },
+
+    removeAt: function (index) {
+        this.items.splice(index, 1);
+    }
+};
